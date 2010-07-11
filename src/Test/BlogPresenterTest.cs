e662439f05ebf94bbacc1ffd600090cffb847419
Declan Whelan <dwhelan@dpwhelan.com>
2010-07-11 11:07:54 -0400

    Added README content and cleaned up BlogPresenterTest.cs

--- src/Test/BlogPresenterTest.cs
+++ src/Test/BlogPresenterTest.cs
@@ -7,81 +7,67 @@ namespace Moq.Sequences.Test
 {
     public class BlogPresenterTest
     {
-        private readonly Post olderPost = new Post { DateTime = new DateTime(2010, 1, 1) };
-        private readonly Post newerPost = new Post { DateTime = new DateTime(2010, 1, 2) };
-        private readonly List<Post> posts;
+[Test]
+public void Should_show_each_post_once_with_most_recent_first()
+{
+    var olderPost = new Post { DateTime = new DateTime(2010, 1, 1) };
+    var newerPost = new Post { DateTime = new DateTime(2010, 1, 2) };
+    var posts = new List<Post> { newerPost, olderPost };
+    var mockView = new Mock<BlogView>();
 
-        public BlogPresenterTest()
-        {
-            posts = new List<Post> { newerPost, olderPost };
-        }
+    var viewOrder = 0;
 
-        [Test]
-        public void Should_show_all_posts()
-        {
-            var mockView = new Mock<BlogView>();
+    mockView.Setup(v => v.ShowPost(newerPost)).Callback(() => Assert.That(viewOrder++, Is.EqualTo(0)));
+    mockView.Setup(v => v.ShowPost(olderPost)).Callback(() => Assert.That(viewOrder++, Is.EqualTo(1)));
 
-            new BlogPresenter(mockView.Object).Show(posts);
+    new BlogPresenter(mockView.Object).Show(posts);
 
-            mockView.Verify(v => v.ShowPost(olderPost), Times.Once());
-            mockView.Verify(v => v.ShowPost(newerPost), Times.Once());
-        }
-    
+    mockView.Verify(v => v.ShowPost(newerPost), Times.Once());
+    mockView.Verify(v => v.ShowPost(olderPost), Times.Once());
+}
 
-        [Test]
-        public void Should_show_posts_with_most_recent_first()
-        {
-            var mockView = new Mock<BlogView>();
+[Test]
+public void Should_show_each_post_with_most_recent_first_using_sequences()
+{
+    var olderPost = new Post { DateTime = new DateTime(2010, 1, 1) };
+    var newerPost = new Post { DateTime = new DateTime(2010, 1, 2) };
+    var posts = new List<Post> { newerPost, olderPost };
 
-            var viewOrder = 0;
+    var mockView = new Mock<BlogView>();
 
-            mockView.Setup(v => v.ShowPost(olderPost)).Callback(() => Assert.That(viewOrder++, Is.EqualTo(0)));
-            mockView.Setup(v => v.ShowPost(newerPost)).Callback(() => Assert.That(viewOrder++, Is.EqualTo(1)));
-
-            new BlogPresenter(mockView.Object).Show(posts);
-
-            mockView.Verify(v => v.ShowPost(olderPost), Times.Once());
-            mockView.Verify(v => v.ShowPost(newerPost), Times.Once());
-        }
-
-        [Test]
-        public void Should_show_posts_with_most_recent_first_using_MockSequence()
-        {
-            var mockView = new Mock<BlogView>();
-
-            using (Sequence.Create())
-            {
-                mockView.Setup(v => v.ShowPost(olderPost)).InSequence();
-                mockView.Setup(v => v.ShowPost(newerPost)).InSequence();
-
-                new BlogPresenter(mockView.Object).Show(posts);
-            }
-        }
-    }
-
-    public class Post
+    using (Sequence.Create())
     {
-        public DateTime DateTime { get; set; }
+        mockView.Setup(v => v.ShowPost(newerPost)).InSequence();
+        mockView.Setup(v => v.ShowPost(olderPost)).InSequence();
+
+        new BlogPresenter(mockView.Object).Show(posts);
+    }
+}
     }
 
-    public class BlogPresenter
+public class Post
+{
+    public DateTime DateTime { get; set; }
+}
+
+public class BlogPresenter
+{
+    private readonly BlogView view;
+
+    public BlogPresenter(BlogView view)
     {
-        private readonly BlogView view;
-
-        public BlogPresenter(BlogView view)
-        {
-            this.view = view;
-        }
-
-        public void Show(IEnumerable<Post> posts)
-        {
-            foreach (var post in posts.OrderBy(post => post.DateTime))
-                view.ShowPost(post);
-        }
+        this.view = view;
     }
 
-    public interface BlogView
+    public void Show(IEnumerable<Post> posts)
     {
-        void ShowPost(Post post);
+        foreach (var post in posts.OrderByDescending(post => post.DateTime))
+            view.ShowPost(post);
     }
+}
+
+public interface BlogView
+{
+    void ShowPost(Post post);
+}
 }
