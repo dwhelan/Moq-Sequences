db4180cff752fd02ae9e3dbe1b96ed1f857eae01
stakx <stakx@eml.cc>
2019-06-03 21:38:00 +0200

    Reduce reflection over Moq internals & fix regressions
    There have been several changes in Moq 4.11's implementation of the
    `Times` value type. One breaks our library:
    
     * `Times` builds its exception message differently. We need to adjust
       for that by doing some more work to get the same kind of message as
       we did earlier.
    
    The other change allows us to reduce our need to reflect over `Times`
    internals, as `Times` is now officially a [from..to] range:
    
     * The private `Times.to` field can be retrieved by deconstruction.
    
     * We can implement our own version of `Times.Verify` instead of call-
       ing Moq's internal version using reflection.

--- src/Moq.Sequences/Step.cs
+++ src/Moq.Sequences/Step.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Text;
 
 namespace Moq.Sequences
 {
@@ -6,21 +7,17 @@ namespace Moq.Sequences
     {
         private readonly Times expectedCount;
         private readonly string action;
-        private readonly int maxCount;
         private int actualCount;
 
         internal bool Started { get { return actualCount > 0; } }
         internal bool Complete { get; private set; }
 
-        private static readonly FieldInfo to = typeof(Times).GetField("to", BindingFlags.Instance | BindingFlags.NonPublic);
-        private static readonly MethodInfo verify = typeof(Times).GetMethod("Verify", BindingFlags.Instance | BindingFlags.NonPublic);
         private static readonly MethodInfo getExceptionMessage = typeof(Times).GetMethod("GetExceptionMessage", BindingFlags.Instance | BindingFlags.NonPublic);
 
         internal Step(Times expectedCount, string action = "")
         {
             this.expectedCount = expectedCount;
             this.action = action;
-            maxCount = (int)to.GetValue(expectedCount);
         }
 
         internal void CountCall()
@@ -28,6 +25,7 @@ namespace Moq.Sequences
             if (Complete)
                 throw new SequenceException(this + " is not invokable because it has already completed.");
 
+            var (_, maxCount) = expectedCount;
             if (++actualCount > maxCount)
                 throw new SequenceException(GetFailureMessage("Exceeded maximum number of invocations."));
         }
@@ -42,12 +40,17 @@ namespace Moq.Sequences
 
         private bool Verified()
         {
-            return (bool) verify.Invoke(expectedCount, new[] { (object)actualCount });
+            var (minCount, maxCount) = expectedCount;
+            return minCount <= actualCount && actualCount <= maxCount;
         }
         
         protected virtual string GetFailureMessage(string failMessage)
         {
-            return (string) getExceptionMessage.Invoke(expectedCount, new object[] { failMessage, ToString(), actualCount });
+            var message = new StringBuilder();
+            message.AppendLine(failMessage);
+            message.Append((string)getExceptionMessage.Invoke(expectedCount, new object[] { actualCount }));
+            message.AppendLine(action);
+            return message.ToString();
         }
 
         internal virtual void Reset()
